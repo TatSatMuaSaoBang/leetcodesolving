adcafaa0e557f1b33a1a2a2210f5e89f49881ee8
TatSatMuaSaoBang <chickenniggen568@gmail.com>
2023-10-21 20:25:16 +0700

    changing something not as much as important

--- coding.py
+++ coding.py
@@ -19,6 +19,9 @@ true=int(truel1) + int(truel2)
 map(int, str(true))
 
 #next problems
+#I can be placed before V (5) and X (10) to make 4 and 9. 
+#X can be placed before L (50) and C (100) to make 40 and 90. 
+#C can be placed before D (500) and M (1000) to make 400 and 900.
 I=1
 V=5
 X=10
